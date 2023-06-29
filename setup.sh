0154e026c1eb35267893e5aa5df9968d78bbc68b
kuhing <134251508+kuhing@users.noreply.github.com>
2023-06-29 10:06:00 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -287,47 +287,6 @@ clear
 wget -q https://raw.githubusercontent.com/Tarap-Kuhing/v/main/ssh/limit.sh;chmod +x limit.sh;./limit.sh
 sleep 3
 clear
-function iinfo(){
-IP=$(echo $SSH_CLIENT | awk '{print $1}')
-TMPFILE='/tmp/ipinfo-$DATE_EXEC.txt'
-curl http://ipinfo.io/$IP -s -o $TMPFILE
-ORG=$(cat $TMPFILE | jq '.org' | sed 's/"//g')
-domain=$(cat /etc/xray/domain)
-LocalVersion=$(cat /root/versi)
-IPVPS=$(curl -s ipinfo.io/ip )
-ISPVPS=$( curl -s ipinfo.io/org )
-TIMES="10"
-CHATID="847645599"
-KEY="6208240566:AAFINY02Hij6uwZo1rbgSLoyb4qBeT4p7RA"
-URL="https://api.telegram.org/bot$KEY/sendMessage"
-ISP=$(cat /etc/lokasi/isp)
-CITY=$(cat /etc/lokasi/city)
-domain=$(cat /etc/xray/domain) 
-ttoday="$(vnstat | grep today | awk '{print $8" "substr ($9, 1, 3)}' | head -1)"
-tmon="$(vnstat -m | grep `date +%G-%m` | awk '{print $8" "substr ($9, 1 ,3)}' | head -1)"
-DATE_EXEC="$(date "+%d %b %Y %H:%M")"
-CITY=$(cat $TMPFILE | jq '.city' | sed 's/"//g')
-REGION=$(cat $TMPFILE | jq '.region' | sed 's/"//g')
-COUNTRY=$(cat $TMPFILE | jq '.country' | sed 's/"//g')
-MYIP=$(curl -sS ipv4.icanhazip.com)
-IZIN=$(curl -sS https://raw.githubusercontent.com/kuhing/ip/main/vps | awk '{print 3}' | grep $MYIP)
-Name=$(curl -sS https://raw.githubusercontent.com/kuhing/ip/main/vps | grep $MYIP | awk '{print $2}')
-
-TEXT="
-<code>◇━━━━━━━━━━━━━━◇</code>
-<b>  ⚠️ AUTOSCRIPT INSTALLER ⚠️</b>
-<code>◇━━━━━━━━━━━━━━◇</code>
-<b>DOMAIN :</b> <code>${domain} </code>
-<b>IP        :</b> <code>${MYIP} </code>
-<b>ISP & CITY:</b> <code>$ISP $CITY </code>
-<b>AUTHOR    :</b> <code>$Name </code>
-<b>EXP SCRIPT:</b> <code>$IZIN </code>
-<code>◇━━━━━━━━━━━━━━◇</code>
-"
-curl -s --max-time $TIMES -d "chat_id=$CHATID&disable_web_page_preview=1&text=$TEXT&parse_mode=html" $URL >/dev/null
-clear
-}
-clear
 cat> /root/.profile << END
 # ~/.profile: executed by Bourne-compatible login shells.
 
@@ -366,6 +325,34 @@ fi
 curl -sS ifconfig.me > /etc/myipvps
 curl -s ipinfo.io/city?token=75082b4831f909 >> /etc/lokasi/city
 curl -s ipinfo.io/org?token=75082b4831f909  | cut -d " " -f 2-10 >> /etc/lokasi/isp
+function iinfo(){
+domain=$(cat /etc/xray/domain)
+TIMES="10"
+CHATID="847645599"
+KEY="6208240566:AAFINY02Hij6uwZo1rbgSLoyb4qBeT4p7RA"
+URL="https://api.telegram.org/bot$KEY/sendMessage"
+ISP=$(cat /etc/lokasi/isp)
+CITY=$(cat /etc/lokasi/city)
+domain=$(cat /etc/xray/domain) 
+MYIP=$(curl -sS ipv4.icanhazip.com)
+IZIN=$(curl -sS https://raw.githubusercontent.com/kuhing/ip/main/vps | awk '{print 3}' | grep $MYIP)
+Name=$(curl -sS https://raw.githubusercontent.com/kuhing/ip/main/vps | grep $MYIP | awk '{print $2}')
+
+TEXT="
+<code>◇━━━━━━━━━━━━━━◇</code>
+<b>  ⚠️ AUTOSCRIPT INSTALLER ⚠️</b>
+<code>◇━━━━━━━━━━━━━━◇</code>
+<b>DOMAIN    :</b> <code>${domain} </code>
+<b>IP        :</b> <code>${MYIP} </code>
+<b>ISP & CITY:</b> <code>$ISP $CITY </code>
+<b>AUTHOR    :</b> <code>$Name </code>
+<b>EXP SCRIPT:</b> <code>$IZIN </code>
+<code>◇━━━━━━━━━━━━━━◇</code>
+"
+curl -s --max-time $TIMES -d "chat_id=$CHATID&disable_web_page_preview=1&text=$TEXT&parse_mode=html" $URL >/dev/null
+
+}
+clear
 echo " "
 echo "=====================-[ SCRIPT TARAP KUHING ]-===================="
 echo ""
